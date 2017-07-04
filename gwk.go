d742b8dc439731b552fe892d6628ac05e8a75991
vCaesar <vzvway@gmail.com>
2017-07-04 21:04:56 +0800

    Update vendor.json

--- gwk.go
+++ gwk.go
@@ -3,6 +3,7 @@ package gwk
 import (
 	// _ "github.com/cznic/kv"
 	_ "github.com/boltdb/bolt"
+	_ "github.com/dgraph-io/badger"
 	_ "github.com/go-ego/gse"
 	_ "github.com/go-ego/murmur"
 	_ "github.com/syndtr/goleveldb/leveldb"
