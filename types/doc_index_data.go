5aa047725be355da5f75a1aad2f3b81074f12ab9
vcaesar <vzvway@gmail.com>
2017-08-23 20:31:25 +0800

    Update .gitignore

--- types/doc_index_data.go
+++ types/doc_index_data.go
@@ -30,6 +30,7 @@ type DocIndexData struct {
 	// 当Content不为空的时候，优先从Content中分词得到关键词。
 	// Tokens存在的意义在于绕过悟空内置的分词器，在引擎外部
 	// 进行分词和预处理。
+	// Tokens []*TokenData
 	Tokens []TokenData
 
 	// 文档标签（必须是UTF-8格式），比如文档的类别属性等，这些标签并不出现在文档文本中
