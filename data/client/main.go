e38a352dc63906520b7605d02d53d95da9860090
vcaesar <vzvway@gmail.com>
2017-10-21 22:11:50 +0800

    add some data

---
+++ data/client/main.go
@@ -0,0 +1,96 @@
+// Copyright 2017 ego authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	pb "github.com/go-ego/riot/net/grpc/riot-pb"
+	grpclb "github.com/go-vgo/grpclb"
+	"google.golang.org/grpc"
+)
+
+var (
+	msg  = "在路上, in the way"
+	serv = flag.String("service", "riot_service", "service name")
+	reg  = flag.String("reg", "http://127.0.0.1:2379", "register etcd address")
+)
+
+func main() {
+	flag.Parse()
+	r := grpclb.NewResolver(*serv)
+	b := grpc.RoundRobin(r)
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	conn, err := grpc.DialContext(ctx, *reg, grpc.WithInsecure(), grpc.WithBalancer(b), grpc.WithBlock())
+	if err != nil {
+		log.Println("dial...", err)
+		// return
+	}
+
+	add(conn)
+	search(conn)
+}
+
+func add(conn *grpc.ClientConn) {
+	client := pb.NewGreeterClient(conn)
+	for i := 0; i < 1000; i++ {
+		resp, err := client.DocInx(context.Background(), &pb.DocReq{
+			DocId:       20,
+			Content:     msg,
+			ForceUpdate: false,
+		})
+		if err != nil {
+			log.Fatalf("could not greet: %v", err)
+			return
+		}
+
+		log.Printf("Greeting: %d", resp.Result)
+	}
+}
+
+func search(conn *grpc.ClientConn) {
+	client := pb.NewGreeterClient(conn)
+	resp, err := client.Search(context.Background(), &pb.SearchReq{
+		Id:    "20",
+		Query: "路上",
+	})
+
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+		return
+	}
+
+	log.Printf("Greeting: %v", resp)
+
+}
+
+func del(conn *grpc.ClientConn) {
+	client := pb.NewGreeterClient(conn)
+	resp, err := client.Delete(context.Background(), &pb.DeleteReq{
+		DocId: 20,
+	})
+
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+		return
+	}
+
+	log.Printf("Greeting: %d", resp.Result)
+}
---
+++ data/riot/main.go
@@ -0,0 +1,67 @@
+// Copyright 2017 ego authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+package main
+
+import (
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/go-ego/riot/net/com"
+	rpc "github.com/go-ego/riot/net/grpc"
+	rhttp "github.com/go-ego/riot/net/http"
+
+	"github.com/go-ego/riot/engine"
+	"github.com/go-vgo/gt/conf"
+	"github.com/go-vgo/gt/zlog"
+)
+
+var (
+	// searcher is coroutine safe
+	searcher = engine.Engine{}
+)
+
+// var config rpc.Config
+var config com.Config
+var wg sync.WaitGroup
+
+func init() {
+	zlog.Init("../conf/log.toml")
+	conf.Init("../conf/riot.toml", &config)
+	go conf.NewWatcher("../conf/riot.toml", &config)
+
+	rpc.InitEngine(config)
+
+	grpcPort := config.Rpc.GrpcPort
+	go rpc.InitGrpc(grpcPort[0])
+	distPort := config.Rpc.DistPort
+	go rpc.InitGrpc(distPort[0])
+
+	go rpc.InitErpc(0)
+}
+
+func main() {
+
+	if config.Engine.Mode == "dev" {
+		go func() {
+			log.Println(http.ListenAndServe("localhost:6060", nil))
+		}()
+	}
+
+	http.HandleFunc("/search", rhttp.Search)
+	http.HandleFunc("/dist", rhttp.WgDist)
+	log.Println("服务器 8080 启动...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
