6ca2281091e4ac6e3a5ceb7548d10d3534805f39
vcaesar <vzvway@gmail.com>
2017-12-30 18:10:12 +0800

    add only id engine test

--- engine_test.go
+++ engine_test.go
@@ -527,6 +527,42 @@ func TestCountDocsOnly(t *testing.T) {
 	utils.Expect(t, "2", outputs.NumDocs)
 }
 
+func TestDocOnlyID(t *testing.T) {
+	var engine Engine
+	engine.Init(types.EngineOpts{
+		Using:         1,
+		IDOnly:        true,
+		SegmenterDict: "./testdata/test_dict.txt",
+		DefaultRankOpts: &types.RankOpts{
+			ReverseOrder:    true,
+			OutputOffset:    0,
+			MaxOutputs:      1,
+			ScoringCriteria: &RankByTokenProximity{},
+		},
+		IndexerOpts: &types.IndexerOpts{
+			IndexType: types.LocsIndex,
+		},
+	})
+
+	AddDocs(&engine)
+	engine.RemoveDoc(5)
+	engine.FlushIndex()
+
+	docIds := make(map[uint64]bool)
+	docIds[5] = true
+	docIds[1] = true
+	outputs := engine.Search(types.SearchReq{
+		Text:   "中国人口",
+		DocIds: docIds})
+
+	if outputs.Docs != nil {
+		outDocs := outputs.Docs.(types.ScoredIDs)
+		utils.Expect(t, "1", len(outDocs))
+	}
+	utils.Expect(t, "2", len(outputs.Tokens))
+	utils.Expect(t, "1", outputs.NumDocs)
+}
+
 func TestSearchWithin(t *testing.T) {
 	var engine Engine
 	engine.Init(types.EngineOpts{
