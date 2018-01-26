72419806fb653c10daa2f711051f4cbf24ed5fd7
vcaesar <vzvway@gmail.com>
2018-01-27 01:32:31 +0800

    Update README.md and examples

--- examples/new/main.go
+++ examples/new/main.go
@@ -14,14 +14,14 @@ var (
 func main() {
 	data := types.DocIndexData{Content: `I wonder how, I wonder why
 		, I wonder where they are`}
-	data1 := types.DocIndexData{Content: "留给真爱你的人"}
-	data2 := types.DocIndexData{Content: "也没有理由"}
+	data1 := types.DocIndexData{Content: "所以, 你好, 再见"}
+	data2 := types.DocIndexData{Content: "没有理由"}
 	searcher.IndexDoc(1, data)
 	searcher.IndexDoc(2, data1)
 	searcher.IndexDoc(3, data2)
 	searcher.FlushIndex()
 
-	req := types.SearchReq{Text: "真爱"}
+	req := types.SearchReq{Text: "你好"}
 	search := searcher.Search(req)
 	log.Println("search...", search)
 }
